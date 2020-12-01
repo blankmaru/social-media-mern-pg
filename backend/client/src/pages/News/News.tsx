fa112727bd48047f7a401d8769027cc58ed9f067
blankmaru <coopskywavy@gmail.com>
2020-12-01 23:57:04 +0200

    Creating side panel for news, create table for people's who added posts...

--- backend/client/src/pages/News/News.tsx
+++ backend/client/src/pages/News/News.tsx
@@ -6,14 +6,18 @@ import {
     Empty,
     List,
     Card,
-    Image
+    Image,
+    Avatar,
+    Carousel
 } from 'antd';
-import { IPost } from '../../interfaces/interfaces'
+import { IPost, IUser } from '../../interfaces/interfaces'
 import Axios, { AxiosResponse } from 'axios';
 import { myContext } from '../../Context';
 
 import AddPost from './AddPost'
 import PostItem from './PostItem'
+import { Link } from 'react-router-dom';
+import { CrownOutlined } from '@ant-design/icons';
 
 
 // raw data template
@@ -47,12 +51,30 @@ const chats = [
     },
 ]
 
+const images = [
+    {
+        src: 'https://i.pinimg.com/originals/e2/bc/e5/e2bce5c32c716244954b3020fe1c695e.jpg'
+    },
+    {
+        src: 'https://i.pinimg.com/originals/e2/bc/e5/e2bce5c32c716244954b3020fe1c695e.jpg'
+    }
+]
+
 const News: React.FC = () => {
     const ctx = useContext(myContext)
     const [posts, setPosts] = useState<Array<IPost>>([])
     const [status, setStatus] = useState<boolean>(false)
 
+    const [peoples, setPeoples] = useState<Array<IUser>>([])
+
     useEffect(() => {
+        Axios.get('http://localhost:5000/api/news/peoples', {
+            withCredentials: true
+        }).then((res: AxiosResponse) => {
+            setPeoples(res.data.reverse())
+            console.log(res.data)
+        })
+
         Axios.get('http://localhost:5000/api/posts', {
             withCredentials: true
         }).then((res: AxiosResponse) => {
@@ -83,54 +105,76 @@ const News: React.FC = () => {
                     </div>
                 </Col>
                 <Col span={4}>
-                    <p>Popular Author's</p>
+        
                     <div>
-                        <p>Photos</p>
-                        <List
-                            grid={{ gutter: 8, column: 2 }}
-                            dataSource={data}
-                            renderItem={item => (
-                            <List.Item>
-                                    <Image 
-                                        src="https://i.pinimg.com/originals/e2/bc/e5/e2bce5c32c716244954b3020fe1c695e.jpg"  
-                                    />
-                            </List.Item>
-                            )}
-                        />
-                    </div>
-                    
-                    <div>
-                        <p>People</p>
-                        <List
-                            grid={{ gutter: 8, column: 2 }}
-                            dataSource={data}
-                            renderItem={item => (
-                            <List.Item>
-                                <Card>
-                                    <a>
-                                        {item.title}
-                                    </a>
-                                </Card>
-                            </List.Item>
-                            )}
-                        />
+                        <Card>
+                            <strong>Popular Author's</strong>
+                            <List
+                                style={{paddingTop: '1rem'}}
+                                grid={{ gutter: 16, column: 4 }}
+                                dataSource={peoples}
+                                renderItem={item => (
+                                    <List.Item>
+                                        <div style={{display: 'flex', alignItems: 'center'}}>
+                                            <CrownOutlined />
+                                            <Link style={{ color: 'gold' }} to={`/profile/${item.username}`}>{item.username}</Link>
+                                        </div>
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
                     </div>
 
-                    <div>
-                        <p>Chat's</p>
-                        <List
-                            grid={{ gutter: 8, column: 2 }}
-                            dataSource={chats}
-                            renderItem={item => (
-                            <List.Item>
-                                <Card>
-                                    <a>
-                                        {item.title}
-                                    </a>
-                                </Card>
-                            </List.Item>
-                            )}
-                        />
+                    <div style={{paddingTop: '1rem'}}>
+                        <Card>
+                            <strong>Photos</strong>
+                            <Carousel autoplay style={{paddingTop: '1rem'}}>
+                                {images.map((image) => {
+                                    return (
+                                        <Image 
+                                            src={image.src}
+                                        />
+                                    )
+                                })
+                                }
+                            </Carousel>
+                        </Card>
+                    </div>
+                    
+                    <div style={{paddingTop: '1rem'}}>
+                        <Card>
+                            <strong>People</strong>
+                            <List
+                                style={{paddingTop: '1rem'}}
+                                grid={{ gutter: 16, column: 4 }}
+                                dataSource={data}
+                                renderItem={item => (
+                                    <List.Item>
+                                        <Avatar size="large">
+                                            <Link style={{color: 'white'}} to={`/profile/${item.title}`}>{item.title}</Link>
+                                        </Avatar>
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
+                    </div>
+
+                    <div style={{paddingTop: '1rem'}}>
+                        <Card>
+                            <strong>Chat's</strong>
+                            <List
+                                style={{paddingTop: '1rem'}}
+                                grid={{ gutter: 8, column: 2 }}
+                                dataSource={chats}
+                                renderItem={item => (
+                                    <List.Item>
+                                        <div>
+                                            <Link style={{color: 'black'}} to={`/messenger`}>{item.title}</Link>
+                                        </div>
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
                     </div>
                 </Col>
             </Row>
